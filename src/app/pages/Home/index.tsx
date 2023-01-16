33ef63757d259ae8d498cadbcb6a88dd20c3de1c
Bob Farias <bobhenrique9@gmail.com>
2023-01-16 11:20:17 -0300

    [ADD] Initial commit of the API request on the Home page

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -1,4 +1,26 @@
+import { useEffect } from "react";
+// import { IDetailedMovieList } from "./../../interfaces";
+
+import { getListMovies } from "../../api";
+
 const Home: React.FC = () => {
+  // TODO: update states from the API
+  // const [movieList, setMovieList] = useState<IDetailedMovieList | undefined>();
+  // const [loadingMovieList, setLoadingMovieList] = useState<boolean>(false);
+
+  useEffect(() => {
+    getListMovies()
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(() => {
+        // TODO: notification of errors
+        // toast.error(
+        //  "It was not possible to find the ranking between the newjoiners."
+        // );
+      });
+  }, []);
+
   return <div>Home page</div>;
 };
 
