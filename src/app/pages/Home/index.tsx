c57bbca1a4c782557b82513485df3b56836b40e8
Bob Farias <bobhenrique9@gmail.com>
2023-02-02 13:46:45 +0100

    [ADD] Making the request to get the data from the API [ADD] created the basic structure for the pagination and for the layout

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -1,27 +1,90 @@
-import { useEffect } from "react";
-// import { IDetailedMovieList } from "./../../interfaces";
+import { useEffect, useState } from "react";
+import { IDetailedMovieList, IPageMovies } from "./../../interfaces";
 
 import { getListMovies } from "../../api";
 
+import styles from "./Home.module.sass";
+import GridComponent from "./GridComponent";
+
 const Home: React.FC = () => {
-  // TODO: update states from the API
-  // const [movieList, setMovieList] = useState<IDetailedMovieList | undefined>();
-  // const [loadingMovieList, setLoadingMovieList] = useState<boolean>(false);
+  const [recordsPerPage] = useState(7);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [listOfPages, setListOfPages] = useState<number[]>([1]);
+  // FETCH DATA
+  const [movieList, setMovieList] = useState<
+    IDetailedMovieList[] | undefined
+  >();
+  const [moviesPerPage, setMoviesPerPage] = useState<IPageMovies | undefined>();
+
+  // PAGINATION LOGIC
+  let indexOfLastRecord: number = -1;
+  let indexOfFirstRecord: number = -1;
+
+  const updateMoviesPerPage = (data: IDetailedMovieList[]): void => {
+    const currentMovieList: IPageMovies | undefined = moviesPerPage;
+
+    const nextPageData: IDetailedMovieList[] = data.slice(
+      indexOfFirstRecord,
+      indexOfLastRecord,
+    );
+
+    const setNewPage: IPageMovies = {
+      ...currentMovieList,
+      [currentPage]: nextPageData,
+    };
+
+    setMoviesPerPage(setNewPage);
+  };
+
+  useEffect(() => {
+    indexOfLastRecord = currentPage * recordsPerPage;
+    indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+
+    if (movieList === undefined || movieList.length === 0) return;
+
+    updateMoviesPerPage(movieList);
+  }, [currentPage]);
 
   useEffect(() => {
     getListMovies()
       .then((data) => {
-        console.log(data);
+        setMovieList(data);
+        updateMoviesPerPage(data);
       })
-      .catch(() => {
-        // TODO: notification of errors
-        // toast.error(
-        //  "It was not possible to find the ranking between the newjoiners."
-        // );
+      .catch((error) => {
+        console.log(error);
       });
   }, []);
 
-  return <div>Home page</div>;
+  function nextPage(): void {
+    const nextPageCounter: number = currentPage + 1;
+
+    setCurrentPage(nextPageCounter);
+    setListOfPages(listOfPages.concat(nextPageCounter));
+  }
+
+  return (
+    <div
+      className={`${styles.body_container_home_page} ${styles.kisi_warm_grey}`}
+    >
+      <div className={styles.container_grids_home_page}>
+        {listOfPages.map((page, index) => {
+          const lastPage: boolean =
+            listOfPages[listOfPages.length - 1] === page;
+
+          return (
+            <GridComponent
+              key={index}
+              pageNumber={page}
+              data={moviesPerPage?.[page]}
+              lastPage={lastPage}
+              nextPage={nextPage}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
